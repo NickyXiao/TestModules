1316424647bbe68e9bca6484c5bba4b0e77ddbc9
Senble <874557566@qq.com>
2017-07-26 11:01:07 +0800

    1、框架搭建（接下来主要实现模块间通讯）

--- app/src/main/java/example/senble/china/com/testmodules/MainActivity.java
+++ app/src/main/java/example/senble/china/com/testmodules/MainActivity.java
@@ -1,8 +1,13 @@
 package example.senble.china.com.testmodules;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,8 +24,23 @@ public class MainActivity extends AppCompatActivity {
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, example.senble.china.com.products.MainActivity.class));
+//                startActivity(new Intent(MainActivity.this, example.senble.china.com.products.MainActivity.class));
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("这是一个通知")
+                        .setContentText("这是通知内容部分");
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(MainActivity.this);
+                taskStackBuilder.addParentStack(SecondActivity.class);
+                taskStackBuilder.addNextIntent(intent);
+
+                PendingIntent pendingIntent =  taskStackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT,null);
+                builder.setContentIntent(pendingIntent);
+                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(MainActivity.this);
+                notificationManagerCompat.notify("",1,builder.build());
             }
         });
+
     }
 }
---
+++ app/src/main/java/example/senble/china/com/testmodules/SecondActivity.java
@@ -0,0 +1,19 @@
+package example.senble.china.com.testmodules;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
